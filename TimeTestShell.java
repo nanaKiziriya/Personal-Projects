505d6b4d2f22db52a653a188ee9ee76f8b2ad68a
nanaKiziriya <144845521+nanaKiziriya@users.noreply.github.com>
2026-05-14 19:02:49 -0400

    Update and rename MergeSort Testing to TimeTestShell.java

--- TimeTestShell.java
+++ TimeTestShell.java
@@ -1,4 +1,4 @@
-class MergeSortTesting{
+class TimeTestShell{
     public static void main(String[] args) {
         int size = 1000;
         Random r = new Random();
@@ -6,18 +6,16 @@ class MergeSortTesting{
         int[] arrN = arr2.clone();
         
         long startTime = System.nanoTime();
-        
+        // test 1
         long endTime = System.nanoTime();
         long timeElapsed2Split = endTime - startTime;
-        System.out.println("2-split: " + timeElapsed2Split); // in nanoseconds
-
-
+        System.out.println("test 1: " + timeElapsed2Split); // in nanoseconds
 
         startTime = System.nanoTime();
-        
+        // test 2
         endTime = System.nanoTime();
         long timeElapsedNSplit = endTime - startTime;
-        System.out.println("n-split: " + timeElapsedNSplit); // in nanoseconds
+        System.out.println("test 2: " + timeElapsedNSplit); // in nanoseconds
 
     }
 }
