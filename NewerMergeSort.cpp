754019399ff26fafdf25aa46a2b878b994df1b6d
nanaKiziriya <144845521+nanaKiziriya@users.noreply.github.com>
2026-05-14 18:32:04 -0400

    Update NewerMergeSort.cpp
    successful test

--- NewerMergeSort.cpp
+++ NewerMergeSort.cpp
@@ -14,10 +14,12 @@ int main() {
     std::random_device rd;   // non-deterministic generator
     std::mt19937 gen(rd());  // to seed mersenne twister.
     std::uniform_int_distribution<> dist(1,100);
-    int n = 10;
-    int arr[n];
-    for(int i=0; i<n; i++) arr[i] = dist(gen);
-    sort(arr,n);
+    for(int n=1; n<100; n++) for(int i=0; i<10; i++){
+        // int n = 7;
+        int arr[n];
+        for(int i=0; i<n; i++) arr[i] = dist(gen);
+        sort(arr,n);
+    }
     return 0;
 }
 
@@ -26,49 +28,62 @@ int main() {
 // my merge sort
 template<typename T>
 void sort(T *arr,int n){
-    print(arr,n,"BEFORE");
+    // print(arr,n,"BEFORE");
     int temp[n];
 	int *A[2] = {arr, temp}; // in stack, will point to the 2 arrays used for back to back merge sort, one of which was the original array
 	int NEW = 0, NWR = 1;
-	// init: for each power i s.t. pre-sorted portions are size 2^1
+	// init: for each power i s.t. pre-sorted portions are size 2^i
 	// bool: stop when size of pre-sorted portions 2^i>=n i.e. i>=log2(n)
 	// incr: flips new and newer array
-	for(int i=0; i<floor(log2(n)) ; i++, NEW^=1, NWR^=1){
+	for(int i=0; i<log2(n) ; i++, NEW^=1, NWR^=1){
 	    int sect = pow(2,i+1); // size of section
-	    print(sect, "SORTING sections of size ");
-	    print(A[NWR],n,"Blank slate");
+	   // print(sect, "SORTING sections of size ");
+	   // print(A[NWR],n,"Blank slate");
 	    // for each section starting from index s (pre-sorted sect size is sectsize/2)
 		for(int s=0; s<n; s+=sect){
 		    int subsect = sect/2; // typical size of presorted subsections
 			int n1 = std::min(n-s,subsect); // actual size of 1st pre-sorted subsect
 			int n2 = std::min(n-s-n1,subsect); // actual size of 2nd . . .
-			std::cout<<"sect index "<<s<<", subsect lens "<<n1<<"+"<<n2<<std::endl;
+// 			std::cout<<"sect index "<<s<<", subsect lens "<<n1<<"+"<<n2<<std::endl;
 			if(n1>0&&n2>0) for(int j=s, k=s+n1; j<s+n1||k<s+n1+n2; ){
-			    print(j+k-s, " fixing index");
-				// A[NWR][j+k-s] = (A[NEW][j]<A[NEW][k]||k==s+n1+n2)? A[NEW][j++] : A[NEW][k++];
-				if(A[NEW][j]<A[NEW][k]||k==s+n1+n2){
-				    A[NWR][j+k-s] = A[NEW][j];
+			 //   print(j+k-s-n1, " fixing index");
+				// A[NWR][j+k-s-n1] = (j<s+n1&&A[NEW][j]<A[NEW][k]||k==s+n1+n2)? A[NEW][j++] : A[NEW][k++];
+				if(j<s+n1&&A[NEW][j]<A[NEW][k]||k==s+n1+n2){
+				    // std::cout<<"j\n";
+				    A[NWR][j+k-s-n1] = A[NEW][j];
 				    j++;
 				}
 				else{
-				    A[NWR][j+k-s] = A[NEW][k];
+				    // std::cout<<"k\n";
+				    A[NWR][j+k-s-n1] = A[NEW][k];
 				    k++;
 				}
 			}
-			else for(int jk=s; jk<s+n1+n2; jk++) A[NWR][jk] = A[NEW][jk];
-			std::cout << "section " << s/sect+1 << "/" << ceil(double(n)/sect) << " sorted: ";
-			print(A[NWR],n);
+			else for(int jk=s; jk<s+n1+n2; jk++){
+			 //   std::cout<<"fixed index "<<jk<<" already\n";
+			    A[NWR][jk] = A[NEW][jk];
+			}
+// 			std::cout << "section " << s/sect+1 << "/" << ceil(double(n)/sect) << " sorted: ";
+// 			print(A[NWR],n);
 		}
-		print(A[NWR],n,"After sort");
+// 		print(A[NWR],n,"After sort");
 	}
 	
-	print(A[NEW],n,"NEW");
-	print(A[NWR],n,"NWR");
-	
-	if(NEW==1) for(int i=0; i<n; i++){
+	if(NEW==1) for(int i=0; i<n; i++){ //if A[1] was just NWR (got flipped at end of loop)
 		arr[i] = A[1][i];
 	}
-	print(arr,n,"SORTED");
+// 	print(arr,n,"SORTED");
+	
+// 	bool failed=false;
+	for(int i=0;i<n-1;i++){
+	    if(arr[i]>arr[i+1]){
+	        std::cout <<"failed.\n";
+	       // failed=true;
+	        break;
+	    }
+	}
+// 	if(!failed) std::cout<<"good sort.\n";
+	
 }
 
 // .....................................................................
